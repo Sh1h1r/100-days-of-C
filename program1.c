49618c98a8cb3531467f43dd64856336db321fdf
Sh1h1r <yshihir@gmail.com>
2025-08-26 16:19:11 +0530

    Add files via upload
    sum of two number

---
+++ program1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int a,b;
+int main(){
+	printf("enter a number:");
+	scanf("%d",&a);
+	printf("enter a number:");
+	scanf("%d",&b);
+	int c=a+b;
+	printf("the sum of a and b is %d",c);
+	return 0;
+}
