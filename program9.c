7755620e1ddb208f647713ed1f1bca40f64f0102
Shihir <yshihir@gmail.com>
2025-08-30 22:33:12 +0530

    Add files via upload

---
+++ program9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float p,r,t,si,ci;
+	printf("enter princial amount:");
+	scanf("%f",&p);
+	printf("enter the rate of interest:");
+	scanf("%f",&r);
+	printf("enter the time in year:");
+	scanf("%f",&t);
+	si=(p*r*t)/100;
+	ci=p*pow((1 + r/100),t);
+	printf("simple interst=Rs. %f\n",si);
+	printf("compound interst= Rs. %f\n",ci);
+	return 0;
+}
