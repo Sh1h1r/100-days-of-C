c2615299d28e8984d0948f6b0ed39c3a4e821678
Shihir <yshihir@gmail.com>
2025-09-01 22:25:55 +0530

    Add files via upload

---
+++ program14.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	char character;
+	printf("enter a character:");
+	scanf("%c",&character);
+	if(character=='a' || character=='e' || character=='i' || character=='o' || character=='u'){
+		printf("the character is a vowel\n");
+	}
+	else if(character=='A'|| character=='E' || character=='I' || character=='O' || character=='U'){
+		printf("the character is a vowel\n");
+	}
+	else{
+		printf("the character is not vowel\n");
+	}
+	return 0;
+}
