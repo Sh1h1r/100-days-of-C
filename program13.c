02a6f9cde55f55cfcf6a6b528383acccd52350cc
Shihir <yshihir@gmail.com>
2025-09-01 22:17:52 +0530

    Add files via upload

---
+++ program13.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int year;
+	printf("enter an year:");
+	scanf("%d",&year);
+	if (year%400==0){
+		printf("the year is a leap year\n");
+	}
+	else if(year%4==0){
+		if(year%100==0){
+			printf("the year is not a leap year\n");
+		}
+		else{
+			printf("the year is a leap year\n");
+		}
+	}
+	else{
+		printf("the year is not a leap year\n");
+	}
+	return 0;
+}
