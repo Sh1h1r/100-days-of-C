5dadfad471b17a22119319328550e76390e8a479
Shihir <yshihir@gmail.com>
2025-11-01 22:44:07 +0530

    Add files via upload

--- question-36.c
+++ question-36.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 int main(){
-	int number, number_2, hcf;
-	printf("enter a number 1:");
-	scanf("%d", &number);
-	printf("enter a number 2:");
-	scanf("%d", &number_2);
-	for(int i = 1; i <= number; i++){
-		for(int j = 1; j <= number; j++ ){
-			if( i * j == number){
-				for(int k = 1; k <= number_2; k++){
-						for(int l = 1; l <= number_2; l++ ){
-							if( k * l == number_2 ){
-								if( k == i ){
-									hcf = k;
-								}
-							}
-					}
-				}
+	int number1, number2, multiple;
+	printf("enter number 1 :");
+	scanf("%d", &number1);
+	printf("enter number 2 :");
+	scanf("%d", &number2);
+	multiple = number1 * number2;
+	for(int i = number1; i <= multiple; i = i + number1){
+		for(int j = number2; j <= multiple; j = j + number2){
+			if( i == j ){
+				printf("the lcm is %d\n", i);
+				break;
 			}
 		}
 	}
-	printf("the highest common factor is:%d\n", hcf);
 	return 0;
 }
