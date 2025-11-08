00c143e4fd6a3d6d61adbf03e9b2bbbf490912d5
Shihir <yshihir@gmail.com>
2025-11-08 17:56:50 +0530

    Add files via upload

---
+++ question-54.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/*
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+space = 1,2,3
+star = 5, 3 , 1
+row=0,1,2
+*/
+int main(){
+	for( int row = 0; row < 4; row++){
+		for( int space = row; space < 3; space++){
+			printf(" ");
+		}
+		int stars = 1 + 2 * row;
+		for( int star = 0; star < stars; star++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	for( int row = 0; row < 3; row++){
+		for( int space = 0; space <= row; space++){
+			printf(" ");
+		}
+		int stars = 5 - 2 * row;
+		for( int star = 0; star < stars; star++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
