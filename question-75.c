9cc5137f73cf25bad984b57ee0fba4d7180892ba
Shihir <yshihir@gmail.com>
2025-11-26 22:24:57 +0530

    Add files via upload

---
+++ question-75.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+
+    scanf("%d %d", &r, &c);
+
+    int A[r][c], B[r][c], Sum[r][c];
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", Sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
