5bcd96b56c44de15dc06fbc0ba90306b772c01be
Shihir <yshihir@gmail.com>
2025-12-03 22:24:44 +0530

    Add files via upload

---
+++ question-83.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int vowels = 0, consonants = 0;
+    int i = 0;
+
+    while(str[i] != '\0' && str[i] != '\n') {
+        char ch = str[i];
+
+        if(ch >= 'A' && ch <= 'Z')
+            ch = ch + 32;
+
+        if((ch >= 'a' && ch <= 'z')) {
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+        i++;
+    }
+
+    printf("Vowels=%d, Consonants=%d\n", vowels, consonants);
+
+    return 0;
+}
